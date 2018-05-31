42c6c7151cca4ea1e3fff94d88b92f5cc222f72f
amberwuwu <amberxixi@163.com>
2018-05-31 16:36:09 +0800

    add

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+require("./style/main.scss")
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,34 +1,74 @@
 <template>
   <div class="index">
-    <el-row class="tac">
-  <el-col :span="12">
-    <el-menu default-active="1-1">
-      <el-submenu :index="index.toString()" v-for='(item,index) in vboxList' :key="index">
-        <div slot="title">
-          <i class="el-icon-location"></i>
-          <span>{{item.vboxName}}不不不{{index.toString()}}</span>
+    <div class="analyze-container">
+      <!-- 可以加上transition 优化视觉   -->
+      <div class="side-bar" ref="sideBar" :class="searchBarFixed?'fixed':''">
+        <div class="navMenu" id="searchBar">
+          <div class="menu-tit">监控系统</div>
+          <div class="menu-group">
+            <div><span class="icon"></span>直传总数</div>
+            <div><span class="icon"></span>调图总数</div>
+            <div><span class="icon"></span>V盒在线数</div>
+          </div>
+          <div class="menu-tit">v盒监控</div>
+          <div class="menu-search">
+            <input type="text" placeholder="请输入..." v-model="searchContent" @keyup.enter="search">
+            <span class="search-btn" @click="search"></span>
+            <div class="scroll-box">
+              <el-scrollbar class="live-scroll">
+                <el-menu :default-active="activeNum">
+                  <el-submenu :index="index.toString()" v-for='(item,index) in vboxList' :key="index">
+                    <div slot="title">
+                      <span>{{item.vboxName}}</span>
+                    </div>
+                    <el-menu-item-group>
+                      <el-menu-item :index="index.toString()+'-'+indexs.toString()" v-for="(items,indexs) in item.vboxMonitorList" :key="indexs"><i class="icon icon1"></i>{{items.indexName}}</el-menu-item>
+                    </el-menu-item-group>
+                  </el-submenu>
+                </el-menu>
+              </el-scrollbar>
+            </div>
+          </div>
         </div>
-        <el-menu-item-group>
-          <el-menu-item :index="index.toString()+'-'+indexs.toString()" v-for="(items,indexs) in item.vboxMonitorList" :key="indexs">{{items.indexName}}啊啊啊{{index.toString()+'-'+indexs.toString()}}</el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
-    </el-menu>
-  </el-col>
-</el-row>
-
+      </div>
+      <div class="panel-box">
+        <div class="right">
+          <div class="tools">
+            <ul>
+              <li @click="swith(3)" v-bind:class="{pitch:switchnum===3}">三列</li>
+              <li>|</li>
+              <li @click="swith(2)" v-bind:class="{pitch:switchnum===2}">两列</li>
+              <li>|</li>
+              <li @click="swith(1)" v-bind:class="{pitch:switchnum===1}">单列</li>
+            </ul>
+            <span class="start"></span>
+            <span class="stop"></span>
+          </div>
+          <div v-bind:class="[{ three: switchnum===3 }, { two: switchnum===2 }, { one: switchnum===1 }]">
+            <div class="close"></div>
+            <div class="chart" ref="myCharts" v-on:mousedown="mousedown($event)" v-on:mouseup="mouseup($event)"></div>
+            <span class="start"></span>
+            <span class="stop"></span>
+            <span class="minus">-</span>
+          </div>
+          <div v-bind:class="[{ three: switchnum===3 }, { two: switchnum===2 }, { one: switchnum===1 }]">2</div>
+          <div v-bind:class="[{ three: switchnum===3 }, { two: switchnum===2 }, { one: switchnum===1 }]">3</div>
+          <div v-bind:class="[{ three: switchnum===3 }, { two: switchnum===2 }, { one: switchnum===1 }]">5</div>
+          <div v-bind:class="[{ three: switchnum===3 }, { two: switchnum===2 }, { one: switchnum===1 }]">6</div>
+          <div v-bind:class="[{ three: switchnum===3 }, { two: switchnum===2 }, { one: switchnum===1 }]">7</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
   name: "Index",
   data() {
     return {
-      vboxList: [
-        {
+      vboxList: [{
           vboxName: "vbox4019",
-          vboxMonitorList: [
-            {
+          vboxMonitorList: [{
               indexName: "cpu监控"
             },
             {
@@ -44,8 +84,7 @@ export default {
         },
         {
           vboxName: "vbox4020",
-          vboxMonitorList: [
-            {
+          vboxMonitorList: [{
               indexName: "cpu监控"
             },
             {
@@ -61,8 +100,7 @@ export default {
         },
         {
           vboxName: "vbox4021",
-          vboxMonitorList: [
-            {
+          vboxMonitorList: [{
               indexName: "cpu监控"
             },
             {
@@ -78,8 +116,7 @@ export default {
         },
         {
           vboxName: "vbox40202",
-          vboxMonitorList: [
-            {
+          vboxMonitorList: [{
               indexName: "cpu监控"
             },
             {
@@ -93,16 +130,37 @@ export default {
             }
           ]
         }
-      ]
+      ],
+      tableData: [{
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+      ],
+      currentRow: null
     };
   },
   mounted() {
     let num = [1, 4, -5, 10].findIndex((value, index, arr) => value === 10); // 2
     console.log(num);
   },
-  methods: {}
+  methods: {
+    setCurrent(row) {
+      this.$refs.singleTable.setCurrentRow(row);
+    },
+    handleCurrentChange(val) {
+      this.currentRow = val;
+    }
+  }
 };
-</script>
 
+</script>
 <style scoped>
+
+
 </style>
