f255c62e3ed4db59b664277146d512f2921efd2a
amberwuwu <amberxixi@163.com>
2018-05-31 13:28:21 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+
+</script>
+<script>
+// daovoice 机器人
+(function(i,s,o,g,r,a,m){i["DaoVoiceObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;a.charset="utf-8";m.parentNode.insertBefore(a,m)})(window,document,"script",('https:' == document.location.protocol ? 'https:' : 'http:') + "//widget.daovoice.io/widget/7d5bffca.js","daovoice")
+daovoice('init', {
+  app_id: "7d5bffca"
+});
+daovoice('update');
+
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="index">
+    <el-row class="tac">
+  <el-col :span="12">
+    <el-menu default-active="1-4">
+      <el-submenu index="1" v-for='(item,index) in vboxList' :key="index">
+        <div slot="title">
+          <i class="el-icon-location"></i>
+          <span>{{vboxList.vboxName}}</span>
+        </div>
+        <el-menu-item-group>
+          <el-menu-item index="1-1" v-for="(items,indexs) in vboxList.vboxMonitorList" :key="indexs">{{items.indexName}}</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+  </el-col>
+</el-row>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      vboxList: [
+        {
+          vboxName: "vbox4019",
+          vboxMonitorList: [
+            {
+              indexName: "aaaa"
+            },
+            {
+              indexName: "bbbb"
+            },
+            {
+              indexName: "cccc"
+            },
+            {
+              indexName: "dddd"
+            }
+          ]
+        },
+         {
+          vboxName: "vbox4020",
+          vboxMonitorList: [
+            {
+              indexName: "aaaa"
+            },
+            {
+              indexName: "bbbb"
+            },
+            {
+              indexName: "cccc"
+            },
+            {
+              indexName: "dddd"
+            }
+          ]
+        }
+      ]
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
