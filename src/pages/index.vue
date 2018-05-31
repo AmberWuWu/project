05c4c891418d22323ae4922555490f9c982681ab
amberwuwu <amberxixi@163.com>
2018-05-31 14:42:16 +0800

    sidebar例子

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,14 +2,14 @@
   <div class="index">
     <el-row class="tac">
   <el-col :span="12">
-    <el-menu default-active="1-4">
-      <el-submenu index="1" v-for='(item,index) in vboxList' :key="index">
+    <el-menu default-active="1-1">
+      <el-submenu :index="index.toString()" v-for='(item,index) in vboxList' :key="index">
         <div slot="title">
           <i class="el-icon-location"></i>
-          <span>{{vboxList.vboxName}}</span>
+          <span>{{item.vboxName}}不不不{{index.toString()}}</span>
         </div>
         <el-menu-item-group>
-          <el-menu-item index="1-1" v-for="(items,indexs) in vboxList.vboxMonitorList" :key="indexs">{{items.indexName}}</el-menu-item>
+          <el-menu-item :index="index.toString()+'-'+indexs.toString()" v-for="(items,indexs) in item.vboxMonitorList" :key="indexs">{{items.indexName}}啊啊啊{{index.toString()+'-'+indexs.toString()}}</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
     </el-menu>
@@ -29,38 +29,76 @@ export default {
           vboxName: "vbox4019",
           vboxMonitorList: [
             {
-              indexName: "aaaa"
+              indexName: "cpu监控"
             },
             {
-              indexName: "bbbb"
+              indexName: "网络延迟"
             },
             {
-              indexName: "cccc"
+              indexName: "sd卡写入"
             },
             {
-              indexName: "dddd"
+              indexName: "直传数量"
             }
           ]
         },
-         {
+        {
           vboxName: "vbox4020",
           vboxMonitorList: [
             {
-              indexName: "aaaa"
+              indexName: "cpu监控"
             },
             {
-              indexName: "bbbb"
+              indexName: "网络延迟"
             },
             {
-              indexName: "cccc"
+              indexName: "sd卡写入"
             },
             {
-              indexName: "dddd"
+              indexName: "直传数量"
+            }
+          ]
+        },
+        {
+          vboxName: "vbox4021",
+          vboxMonitorList: [
+            {
+              indexName: "cpu监控"
+            },
+            {
+              indexName: "网络延迟"
+            },
+            {
+              indexName: "sd卡写入"
+            },
+            {
+              indexName: "直传数量"
+            }
+          ]
+        },
+        {
+          vboxName: "vbox40202",
+          vboxMonitorList: [
+            {
+              indexName: "cpu监控"
+            },
+            {
+              indexName: "网络延迟"
+            },
+            {
+              indexName: "sd卡写入"
+            },
+            {
+              indexName: "直传数量"
             }
           ]
         }
       ]
     };
+  },
+  mounted() {
+    let num = [1, 4, -5, 10].findIndex((value, index, arr) => value === 10); // 2
+    console.log(num);
   },
   methods: {}
 };
