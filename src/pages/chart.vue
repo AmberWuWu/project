0a74b343775d84131e3a71e70cbe452ba9f2cfa7
wq吴琼(内容信息研发部) <qwub@ctrip.com>
2018-06-26 17:24:31 +0800

    add

--- src/pages/chart.vue
+++ src/pages/chart.vue
@@ -1,214 +1,95 @@
 <template>
   <div class="chart">
     <my-header></my-header>
-    <div class="chart" ref="myCharts"></div>
+    <el-table ref="multipleTable" :data="tableData3" tooltip-effect="dark" style="width: 100%">
+    <el-table-column type="selection" width="55"></el-table-column>
+    <el-table-column label="日期" width="120">
+      <template slot-scope="scope">{{ scope.row.date }}</template>
+    </el-table-column>
+    <el-table-column prop="name" label="姓名" width="120">
+    </el-table-column>
+    <el-table-column prop="address" label="地址" show-overflow-tooltip>
+    </el-table-column>
+  </el-table>
+  <div style="margin-top: 20px">
+    <!-- <el-button @click="toggleSelection([tableData3[1], tableData3[2]])">切换第二、第三行的选中状态</el-button> -->
+    <el-button @click="toggleSelection(1)">取消选择</el-button>
+    <el-button @click="toggleSelection(2)">全选</el-button>
+  </div>
   </div>
 </template>
 <script>
-import myHeader from '../components/header';
-// import chart from "vue-echarts/components/ECharts.vue";
-import "echarts/lib/chart/pie";
-import "echarts/lib/component/tooltip";
+import myHeader from "../components/header";
+
 const echarts = require("echarts");
 export default {
   name: "Chart",
   data() {
     return {
-      option: {
-        color: colors,
-        title: {
-          text: "多条数据",
-          color: "#19233C",
-          fontSize: "16px",
-          x: 'center',
+      tableData3: [
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
         },
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'cross'
-          }
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
         },
-        grid: {
-          right: '20%'
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
         },
-        xAxis: [{
-          axisLine: {
-            lineStyle: {
-              color: "#E5E9F2",
-              width: 2
-            }
-          },
-          axisLabel: {
-            show: true,
-            formatter: function(params) {
-              let newParamsName = "";
-              let paramsNameNumber = params.length;
-              let provideNumber = 10; // 一行显示几个字
-              let rowNumber = Math.ceil(paramsNameNumber / provideNumber);
-              if (paramsNameNumber > provideNumber) {
-                for (let p = 0; p < rowNumber; p++) {
-                  let tempStr = "";
-                  let start = p * provideNumber;
-                  let end = start + provideNumber;
-                  if (p == rowNumber - 1) {
-                    tempStr = params.substring(start, paramsNameNumber);
-                  } else {
-                    tempStr = params.substring(start, end) + "\n";
-                  }
-                  newParamsName += tempStr;
-                }
-              } else {
-                newParamsName = params;
-              }
-              return newParamsName;
-            }
-          },
-          data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
-        }],
-        yAxis: [{
-            type: 'value',
-            name: '蒸发量',
-            position: 'right',
-            axisLine: {
-              lineStyle: {
-                color: '#E5E9F2',
-                width: 2
-              }
-            },
-            axisLabel: {
-              formatter: '{value}'
-            }
-          },
-          {
-            type: 'value',
-            name: '降水量',
-            position: 'right',
-            offset: 80,
-            axisLine: {
-              lineStyle: {
-                color: '#E5E9F2',
-                width: 2
-              }
-            },
-            axisLabel: {
-              formatter: '{value}'
-            }
-          },
-          {
-            type: 'value',
-            name: '温度',
-            min: 0,
-            max: 25,
-            position: 'left',
-            axisLine: {
-              lineStyle: {
-                color: '#E5E9F2',
-                width: 2
-              }
-            },
-            axisLabel: {
-              formatter: '{value}'
-            }
-          }, {
-            type: 'value',
-            name: '邮件营销',
-            position: 'right',
-            axisLine: {
-              lineStyle: {
-                color: '#E5E9F2',
-                width: 2
-              }
-            },
-            axisLabel: {
-              formatter: '{value}'
-            }
-          }
-        ],
-        series: [{
-            name: '蒸发量',
-            type: 'line',
-            symbolSize: 2, //拐点大小
-            itemStyle: {
-              normal: {
-                lineStyle: {
-                  width: 1 //折线宽度
-                }
-              }
-            },
-            yAxisIndex: 0,
-            data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-          },
-          {
-            name: '降水量',
-            type: 'line',
-            symbolSize: 2, //拐点大小
-            itemStyle: {
-              normal: {
-                lineStyle: {
-                  width: 1 //折线宽度
-                }
-              }
-            },
-            yAxisIndex: 1,
-            data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
-          },
-          {
-            name: '平均温度',
-            type: 'line',
-            symbolSize: 2, //拐点大小
-            itemStyle: {
-              normal: {
-                lineStyle: {
-                  width: 1 //折线宽度
-                }
-              }
-            },
-            yAxisIndex: 2,
-            data: [0.00, 0.02, 0.3, 0.5, 0.3, 0.2, 0.3, 3.4, 23.0, 16.5, 10.00, 6.2]
-          }, {
-            name: '邮件营销',
-            type: 'line',
-            symbolSize: 2, //拐点大小
-            itemStyle: {
-              normal: {
-                lineStyle: {
-                  width: 1 //折线宽度
-                }
-              }
-            },
-            yAxisIndex: 3,
-            data: [5120, 5132, 5101, 5134, 5090, 5230, 5210, 5220, 5182, 5191, 5234, 5290, ]
-          },
-        ]
-      }
-
-    }
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-08",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-06",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-07",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        }
+      ],
+      multipleSelection: []
+    };
   },
   components: {
-    myHeader,
+    myHeader
   },
-  mounted() {
-    this.initChart();
-  },
+  mounted() {},
   methods: {
-    initChart() {
-      let myChart = echarts.init(this.$refs.myCharts);
-      myChart.setOption(this.option);
-      setTimeout(function() {
-        myChart.resize();
-        window.addEventListener('resize', function() {
-          myChart.resize();
-        })
-      }, 200);
+    toggleSelection(n) {
+      if (n === 1) {
+        // 取消全选
+        this.$refs.multipleTable.clearSelection();
+      }
+      if (n === 2) {
+        // 全选
+        this.$refs.multipleTable.clearSelection();
+        this.tableData3.forEach(row => {
+          this.$refs.multipleTable.toggleRowSelection(row);
+        });
+      }
     },
-  },
+  }
 };
-
 </script>
 <style scoped>
 .chart {
   width: 100%;
   height: 400px;
 }
-
 </style>
